84e5bf99aaf176a4af60d73e7d6dcc45f17b39f3
ChaeEun Lee <taylor.lee1017@gmail.com>
2025-02-11 09:28:50 +0900

    Update downstream.sh

--- downstream.sh
+++ downstream.sh
@@ -19,7 +19,17 @@ python main.py --task downstream --data freesolv --criterion rmse --lr 1e-4 --nu
 # Lipophilicity
 python main.py --task downstream --data lipophilicity --criterion rmse --lr 5e-5 --num_classes 1
 
+# bace
+python main.py --task downstream --data bace --criterion bce --split scaffold --lr 5e-5 --batch_size 32 --num_classes 1 --split scaffold
 
+# sider
+python main.py --task downstream --data sider --criterion bce --num_classes 27 --lr 5e-5
+
+# qm7
+python main.py --task downstream --data qm7 --criterion mae --batch_size 1 --lr 5e-6
+
+# qm8
+python main.py --task downstream --data qm8 --criterion mae --num_classes 12 --batch_size 8 --lr 6e-5 
 
 
 
